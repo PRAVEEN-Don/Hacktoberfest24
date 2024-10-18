73986133a41b05357eea4578a9c6a317ed07ea5c
PRAVEEN-Don <117267706+PRAVEEN-Don@users.noreply.github.com>
2024-10-19 00:12:23 +0530

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -23,14 +23,8 @@ def partition(array, low, high):
             # If element smaller than pivot is found
             # swap it with the greater element pointed by i
             i = i + 1
-
-            # Swapping element at i with element at j
             (array[i], array[j]) = (array[j], array[i])
-
-    # Swap the pivot element with the greater element specified by i
     (array[i + 1], array[high]) = (array[high], array[i + 1])
-
-    # Return the position from where partition is done
     return i + 1
 
 # function to perform quicksort
@@ -38,16 +32,8 @@ def partition(array, low, high):
 
 def quickSort(array, low, high):
     if low < high:
-
-        # Find pivot element such that
-        # element smaller than pivot are on the left
-        # element greater than pivot are on the right
         pi = partition(array, low, high)
-
-        # Recursive call on the left of pivot
         quickSort(array, low, pi - 1)
-
-        # Recursive call on the right of pivot
         quickSort(array, pi + 1, high)
 
 
